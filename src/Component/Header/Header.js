c3d5dc9ea08cd94d1f0fba821778da7d67c497da
AhshanHabib26 <habib.net27@gmail.com>
2022-03-25 21:32:41 +0600

    Project Setup

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {BsCart4} from 'react-icons/bs'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <nav className='nav_container'>
+                <div className="site_name">
+                    <h3> <span> NH Heal</span>th Care</h3>
+                </div>
+                <div className="menu_item">
+                    <BsCart4/>
+                </div>
+            </nav>
+
+            <div className="header_info">
+                <h3>Choose Your Dieting Food</h3>
+            </div>
+
+
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/Component/Products/Products.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+
+const Products = () => {
+
+    const [products, setProducts] = useState([])
+
+    useEffect( () =>{
+        fetch('data.json')
+        .then(res => res.json())
+        .then(data => setProducts(data))
+    }, [])
+
+
+    return (
+        <div>
+           {
+               products.map( product => console.log(product))
+           }
+        </div>
+    );
+};
+
+export default Products;
