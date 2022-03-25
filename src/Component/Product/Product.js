885c0f4875df2299a1b42d4369cb8ca24f5d537c
AhshanHabib26 <habib.net27@gmail.com>
2022-03-25 22:05:42 +0600

    Single Product Show UI

--- src/Component/Product/Product.js
+++ src/Component/Product/Product.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import './Product.css'
 
 const Product = (props) => {
+console.log(props.product)
+
+const {img, name, price} = props.product
+
+
+
     return (
         <div>
-            <h1>{props.product.name}</h1>
+        <div className="product_container">
+            <div className="product_img">
+                <img src={img} alt="" />
+            </div>
+            <div className="product_info">
+                <h3>Name: {name}</h3>
+                <p>Price: $ {price}</p>
+            </div>
+        </div>
         </div>
     );
 };
